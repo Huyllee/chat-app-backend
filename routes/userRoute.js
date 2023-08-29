8234f66ecbc4c2445f64e8fa71fe96c79341cdd0
Huyllee <moriarty1807@gmail.com>
2023-08-29 21:49:03 +0700

    create fetch user api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  findUser,
+  getUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/find/:userId", findUser);
+router.get("/", getUser);
 
 export default router;
